08db325e7410b210f4b691f8da3548da1a7c059b
Miyuki <108064810+ito-miyuki@users.noreply.github.com>
2023-11-29 17:25:44 +0200

    added the first code to test if it

--- get_next_line_not_cloned/get_next_line.c
+++ get_next_line_not_cloned/get_next_line.c
@@ -6,11 +6,43 @@
 /*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 17:30:15 by mito              #+#    #+#             */
-/*   Updated: 2023/11/27 17:31:08 by mito             ###   ########.fr       */
+/*   Updated: 2023/11/29 17:20:05 by mito             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
+    static char    buffer[BUFFER_SIZE + 1];
+    int             chars_read;
+    char            *str; 
+
+ // add whatever is in buffer to str, or put ""
+ //strcpy() <=> strjoin(str, s1, "") - this is how to copy some string using function strjoin
+    while (*str != '\n')
+    {
+        chars_read = read(fd, buffer, BUFFER_SIZE);
+        
+        if (chars_read  <= 0)
+            return NULL;
+    }
+    //buffer[chars_read] = '\0';
+    return (buffer);
 }
 
+#include <stdio.h>
+
+int main() {
+    int fd;
+
+    char *line = get_next_line(fd);
+
+    if (line != NULL) {
+        printf("Read line: %s\n", line);
+    } else {
+        printf("Error reading line or EOF reached.\n");
+    }
+
+    return (0);
+}
---
+++ get_next_line_not_cloned/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 09:10:21 by mito              #+#    #+#             */
+/*   Updated: 2023/11/29 17:00:43 by mito             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
